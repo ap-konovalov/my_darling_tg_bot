1dfd3f5bd6f0980e741a26c1eac58925254f5ebe
Aleksandr Konovalov <work.apkonovalov@gmail.com>
2022-01-16 01:20:42 +0300

    add commands + try fix app run

--- src/main/java/com/mydarligvalerie/command/ComplimentCommand.java
+++ src/main/java/com/mydarligvalerie/command/ComplimentCommand.java
@@ -8,11 +8,14 @@ import java.util.Random;
 
 import static java.util.Arrays.asList;
 
-public class ComplimentCommand implements Command{
+public class ComplimentCommand implements Command {
 
     private final SendBotMessageService sendBotMessageService;
+    private final List<String> compliments = createComplimentsList();
+    private final List<String> introductions = createIntroductionsList();
+    private Random random = new Random();
 
-    public ComplimentCommand(SendBotMessageService sendBotMessageService){
+    public ComplimentCommand(SendBotMessageService sendBotMessageService) {
         this.sendBotMessageService = sendBotMessageService;
     }
 
@@ -21,23 +24,27 @@ public class ComplimentCommand implements Command{
         sendBotMessageService.sendMessage(update.getMessage().getChatId().toString(), getRandomCompliment());
     }
 
-    private List<String> createComplimentsList(){
+    private List<String> createComplimentsList() {
         return asList("умна", "красива", "богата", "солнечна", "жизнерадостна", "привлекательна", "весела",
                 "сообразительна", "активна", "профессиональна", "спокойна", "не агрессивна", "сильна", "интеллигентна",
                 "хорошо воспитана", "пластична", "скромна", "гибка", "хитра", "сексуальна", "стройна", "заботлива",
                 "хороша собой", "вежлива", "рассудительна", "не банальна", "хозяйственна", "проворна", "начитана",
-                "образована", "лингвистична", "здорова");
+                "образована", "лингвистична", "здорова", "обворожительна", "мила", "добра", "аристократична",
+                "бесподобна", "безупречна", "волшебна", "грациозна", "горяча", "гармонична", "деликатна",
+                "желанна", "женственна", "заманчива", "идеальна", "изумительна", "саркастична", "изыскана",
+                "красноречива", "ласкова", "любезна", "мудра", "щедра");
     }
 
-    private List<String> createIntroductionsList(){
+    private List<String> createIntroductionsList() {
         return asList("Дорогая Лера", "Любимая Лера", "Птичка моя", "Лерочка", "Уважаемая Валерия", "Радость моя", "Лера",
-                "Свет моих очей", "Солнце мое", "Моя ненаглядная", "Принцесса моя");
+                "Свет моих очей", "Солнце мое", "Моя ненаглядная", "Принцесса моя", "Золотая моя", "Красавица моя",
+                "(араб.)فاليريا", "Валерыя(белорусское)", "Valéria(венгерское)", "Βαλέρια(греческое)",
+                "Waleria(польское)", "Valiére(французское)", "Валерија(сербское)", "Валерія(украинское)");
     }
 
-    private String getRandomCompliment(){
-        Random random = new Random();
-        List<String> compliments = createComplimentsList();
-        List<String> introductions = createIntroductionsList();
-        return introductions.get(random.nextInt(introductions.size())) + ", ты " + compliments.get(random.nextInt(compliments.size()));
+    private String getRandomCompliment() {
+        String introduction = introductions.get(random.nextInt(introductions.size()));
+        String compliment = compliments.get(random.nextInt(compliments.size()));
+        return introduction + ", ты " + compliment;
     }
 }
